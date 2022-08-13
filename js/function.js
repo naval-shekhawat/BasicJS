5a26c6fededf5e958cb3c048ff36f41a702b6eb1
kush0709 <kaushal100singh@gmail.com>
2022-08-13 20:44:00 +0530

    update

--- js/function.js
+++ js/function.js
@@ -1,6 +1,27 @@
 (function(){
 
-    console.log(a);
-    var a;
+    // function checkAdhar(openAccount) {
+    //     var AadharValid = apiResponse;
+    //     if(AadharValid) {
+    //         openAccount(;)
+    //     }
+    //     else {
+
+    //     }
+    // }
+
+
+    function outerFunction(addFunc) {
+        var a = 100;
+        var b = 50;
+        var output = addFunc(a,b);
+        console.log(output)
+    }
+
+    var add = function(a, b){
+        return a + b;
+    };
+
+    outerFunction(add);
 
 })();
