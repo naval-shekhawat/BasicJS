dcdec478424a495727a62395f24ce4c41219b84f
manjulasivasankar <manjulasivasankar@yahoo.co.in>
2022-08-17 21:53:40 +0530

    fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+(function(){
+
+    function fibonacci(number){
+        let previous=0, current=1, output=0;
+        console.log(previous);
+        console.log(current);
+        for(let i = 1; i < 5; i++){
+           
+            output = previous + current;
+            console.log(output);
+            previous = current;
+            current = output;
+               
+
+        }
+    }
+    console.log(fibonacci(5));
+
+})();
