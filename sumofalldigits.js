4b5f1ea3914d9de389e1f700b42e337bef9b828f
sushovan1999 <sushovan1999@gmail.com>
2022-08-17 21:14:21 +0530

    sumalldigit

---
+++ sumofalldigits.js
@@ -0,0 +1,14 @@
+(function(){
+
+    function getSumofdigits(num){
+        let sum = 0;
+        while(num > 1){
+            num % 10;
+            sum = sum + (num % 10);
+            num = parseInt(num / 10);
+
+        }
+        return sum;
+    }
+    console.log(getSumofdigits(103));
+})();
