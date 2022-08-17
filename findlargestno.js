cd1c6c5f388c3010742831ec76d45e259e8a356e
sushovan1999 <sushovan1999@gmail.com>
2022-08-17 20:46:12 +0530

    find largest no

---
+++ findlargestno.js
@@ -0,0 +1,16 @@
+(function(){
+
+    function findLargestno(first, second, third){
+        if(first >= second && first >= third){
+            return first;
+        }else if(second >= first && second >= third){
+            return second;
+        }else {
+            return third;
+        }
+    }
+
+    console.log(findLargestno(10,20,30));
+    console.log(findLargestno(12,28,35));
+    console.log(findLargestno(5,220,305));
+})();
