9a2b09bd1505aeb62aa16fb32bdd4c7e6d113bda
pragyakumar17 <110718357+pragyakumar17@users.noreply.github.com>
2022-08-19 21:05:56 +0530

    getInnerDigitsSum() function

--- problem3.js
+++ problem3.js
@@ -1,19 +1,18 @@
 (function(){
-    function checkNumber(num){
-        switch (true) {
-            case num > 0:
-                console.log('POSITIVE NUMBER')
-                break;
-            case num < 0:
-                console.log('NEGATIVE NUMBER')
-                break;
-            default:
-                console.log('ZERO')
-                break;
-        }
-    }
-    console.log(checkNumber(-8));
-    console.log(checkNumber(-88));
-    console.log(checkNumber(0));
-    console.log(checkNumber(66));
+    function getInnerDigitsSum(num){
+        if(num.toString().length<=2){
+            console.log('No Inner digit')}
+            else {
+                let sum = 0;
+            num=parseInt(num/10);
+            sum = sum + (num%10);
+            while(parseInt(num/100)!=0){
+                num = parseInt(num /10);
+                sum = sum + (num%10);
+            }
+            return sum;
+        }}
+        console.log(getInnerDigitsSum(9021));
+        console.log(getInnerDigitsSum(987654));
+        console.log(getInnerDigitsSum(91));
 })();
