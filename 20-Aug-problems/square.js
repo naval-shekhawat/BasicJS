842bffab75e00c8096343b435b1328bbd5af7fbe
naval-shekhawat <vijay.j.shekhawat@gmail.com>
2022-08-20 22:05:25 +0530

    20th august

---
+++ 20-Aug-problems/square.js
@@ -0,0 +1,10 @@
+function square(num) {
+    return num * num;
+}
+
+function calculate() {
+    let usrInput = document.getElementById("number").value;// getting the value
+    let output = square(usrInput);
+    let outputElement = document.getElementById("output");
+    outputElement.value = output;  // setting the value
+}
---
+++ 20-Aug-problems/trailingzeroes.js
@@ -0,0 +1,17 @@
+(function(){
+
+     function getTrailingZeroes(num) {
+        let zeroes = 0;
+
+        for(let i = 5;  parseInt(num/i) >= 1; i = i * 5) {
+            zeroes+= parseInt(num/i);
+        }
+
+        return zeroes;
+     }
+
+     console.log(getTrailingZeroes(5));
+     console.log(getTrailingZeroes(10));
+     console.log(getTrailingZeroes(8));
+     console.log(getTrailingZeroes(4));
+})();
