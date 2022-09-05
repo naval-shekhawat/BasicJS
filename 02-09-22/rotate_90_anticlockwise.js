797a0bd58b3ba582d56a2355fd78925b5f332ca6
sushovan1999 <sushovan1999@gmail.com>
2022-09-06 00:21:51 +0530

    02-09-22

--- 02-09-22/rotate_90_anticlockwise.js
+++ 02-09-22/rotate_90_anticlockwise.js
@@ -1,4 +1,12 @@
 (function(){
+    function swap(arr, i ,j) {
+        let temp;
+        temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+    }
+
+
     function transpose(matrix, total_rows, total_cols) {
         for(let row =0; row<total_rows; row++){
             for(let col = 0 ; col < row; col++){
